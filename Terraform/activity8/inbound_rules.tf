a5d59f66877b7d9d5eb50f6bc197a9e1f905ee44
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-03 18:42:32 +0530

    tf8

--- Terraform/activity8/inbound_rules.tf
+++ Terraform/activity8/inbound_rules.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group_rule" "inbound" {
     count = length(var.port)
     type = "ingress"
+    description = "inbound rule for ${var.service[count.index]}"
     security_group_id = aws_security_group.sec_group.id
     from_port = var.port[count.index]
     to_port = var.port[count.index]
@@ -8,3 +9,4 @@ resource "aws_security_group_rule" "inbound" {
     cidr_blocks = ["0.0.0.0/0"]
   
 }
+
