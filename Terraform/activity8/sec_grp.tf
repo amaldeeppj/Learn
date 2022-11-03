4e85f66ac60fe1cacaeab8e4b1467fe92849ea0e
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-03 18:33:06 +0530

    tf8

---
+++ Terraform/activity8/sec_grp.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "sec_group" {
+  count = length(var.service)
+  name        = "sec_grp_TF"
+  description = "configure ports"
+  vpc_id      = aws_vpc.new_vpc_tf.id
+
+#   ingress {
+#     description      = "incoming port for ${var.service[count.index]}"
+#     from_port        = var.port[count.index]
+#     to_port          = var.port[count.index]
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+#   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "tf-secGrp_${var.environment}"
+  }
+}
