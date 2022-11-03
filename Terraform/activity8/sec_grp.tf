d091aa54191c92eeb3723c1a3cb16e9401bb92ba
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-03 18:46:05 +0530

    tf8

--- Terraform/activity8/sec_grp.tf
+++ Terraform/activity8/sec_grp.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "sec_group" {
-  count = length(var.service)
+#   count = length(var.service)
   name        = "sec_grp_TF"
   description = "configure ports"
   vpc_id      = aws_vpc.new_vpc_tf.id
