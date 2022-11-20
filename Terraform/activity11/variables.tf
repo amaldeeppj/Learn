9417800cd090c1f3e98d3ed6082e18ad3d12439e
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-21 00:29:40 +0530

    tf

---
+++ Terraform/activity11/variables.tf
@@ -0,0 +1,13 @@
+variable "CIDR" {
+    description = "variable CIDR"
+    type = string
+    default = "172.16.0.0/16"
+  
+}
+
+variable "Region" {
+    description = "AWS region"
+    type = string
+    default = "us-west-2"
+  
+}
