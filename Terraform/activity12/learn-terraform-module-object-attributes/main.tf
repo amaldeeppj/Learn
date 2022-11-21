be1999a31a0a8eb898a42e2008ede367215c2697
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-21 12:59:58 +0530

    tf

---
+++ Terraform/activity12/learn-terraform-module-object-attributes/main.tf
@@ -0,0 +1,23 @@
+# Terraform configuration
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "module-object-attributes"
+    }
+  }
+}
+
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website"
+
+  bucket_prefix = "module-object-attributes-"
+
+  tags = {
+    terraform     = "true"
+    environment   = "dev"
+    public-bucket = true
+  }
+}
